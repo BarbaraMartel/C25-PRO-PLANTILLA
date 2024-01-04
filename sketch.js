092129683bc1b6170efb8539c9d992c38eff92b3
Barbara <95110916+BarbaraMartel@users.noreply.github.com>
2024-01-03 19:54:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,70 +1,62 @@
-
+//Importar módulos del motor físico Matter.js
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+//variable botón
 var btn2;
-
+//variable ángulo
 var angle=60;
 
 
 function setup() {
   createCanvas(400,400);
 
+  //Crear motor físico y el mundo
   engine = Engine.create();
   world = engine.world;
-  
-   var ball_options = {
-    restitution: 0.95,
-    frictionAir:0.01
-  }
-   
-   var ground_options ={
-     isStatic: true
-   };
-  
-   btn2 = createImg('up.png');
+
+  //Propiedades físicas de la pelota
+
+
+  //Propiedades físicas del suelo
+
+
+  //Crear pelota y agregar al mundo
+
+
+  //Crear suelo y agregar al mundo
+
+
+  //rectMode y ellipseMode
+
+
+  //Crear botón de fuerza
+  btn2 = createImg('up.png');
   btn2.position(350,30);
   btn2.size(50,50);
   btn2.mouseClicked(vForce);
 
-  ground1 = Bodies.rectangle(100,300,100,20,ground_options);
-  World.add(world,ground1);
+  //Crear ventiladores
 
-  ball = Bodies.circle(100,10,20,ball_options);
-  World.add(world,ball);
+
+
   
- 
-  ground = Bodies.rectangle(100,400,650,20,ground_options);
-  World.add(world,ground); 
-  
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
 }
 
 
 function draw() 
 {
   background(51);
+  //Actualizar el motor físico
   Engine.update(engine);
-  
-  
-  Matter.Body.rotate(ground1,angle)
- push();
-  translate(ground1.position.x,ground1.position.y);
-  rotate(angle);
-  rect(0,0,100,20);
-  pop();
-  
-  angle +=0.1;
 
- 
 
-  ellipse(ball.position.x,ball.position.y,20);
-  rect(ground.position.x,ground.position.y,650,20);
- 
-//console.log(ground.position.y);
+  //Mostrar pelota con elipse y suelo con rect
+
+
+  //Mostrar ventiladores
 
   
   
@@ -72,6 +64,7 @@ function draw()
 
 function vForce()
 {
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+  //Función applyForce
+  
 }
   
